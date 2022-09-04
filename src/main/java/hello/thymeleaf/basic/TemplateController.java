70a6fa279075994de17057b41fb9d2d3076e6dd0
Dragonspine <ghoul300@naver.com>
2022-09-04 23:11:22 +0900

    feat: template fragment

---
+++ src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -0,0 +1,27 @@
+package hello.thymeleaf.basic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * packageName    : hello.thymeleaf.basic
+ * fileName       : TemplateController
+ * author         : kanghyun Kim
+ * date           : 2022/09/04
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022/09/04        kanghyun Kim      최초 생성
+ */
+
+@Controller
+@RequestMapping("/template")
+public class TemplateController {
+
+    @GetMapping("/fragment")
+    public String template() {
+        return "template/fragment/fragmentMain";
+    }
+}
