4d0064f11d6ab3fa48564068bc177278f318d8be
Dragonspine <ghoul300@naver.com>
2022-09-05 00:02:52 +0900

    feat: template layout2

--- src/main/java/hello/thymeleaf/basic/TemplateController.java
+++ src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -30,4 +30,9 @@ public class TemplateController {
         return "template/layout/layoutMain";
     }
 
+    @GetMapping("/layoutExtend")
+    public String layoutExtend() {
+        return "template/layoutExtend/layoutExtendMain";
+    }
+
 }
